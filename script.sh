f303bfd4c533197d01660585eeed34b59a9cf6d4
FelipeKha <felipe.kharaba@icloud.com>
2022-12-05 12:44:33 +0100

    Add answer to script

--- script.sh
+++ script.sh
@@ -4,4 +4,7 @@ spawn ssh root@104.248.194.185
 expect "Enter passphrase for key '/Users/felipekharaba/.ssh/id_rsa'" {
     send "vtncomingtolecreusot\r"
 }
+expect "Are you sure you want to continue connecting" {
+    send "yes\r"
+}
 interact
