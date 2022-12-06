75bb2d119dd8bd588a7a67f70bc3301425893a2a
FelipeKha <felipe.kharaba@icloud.com>
2022-12-06 19:05:48 +0100

    Add the fingerprint script again

--- deploy_script.sh
+++ deploy_script.sh
@@ -3,21 +3,11 @@
 set timeout 60
 
 spawn ssh root@104.248.194.185
-# spawn ssh root@104.248.194.185 'bash -s' < remote_script.sh
-# spawn ssh -T root@104.248.194.185 << _remote_commands
-# date >> timestamp.txt
-
-# rm -rf vote_the_news
-# mkdir vote_the_news
-# cd vote_the_news
-# git clone https://github.com/FelipeKha/vote_the_news.git
-
-# _remote_commands
 
 expect "Enter passphrase for key '/Users/felipekharaba/.ssh/id_rsa'"
 send -- "vtncomingtolecreusot\r"
-# expect "Are you sure you want to continue connecting"
-# send -- "yes\r"
+expect "Are you sure you want to continue connecting"
+send -- "yes\r"
 expect "root@docker-s-1vcpu-1gb-ams3-01:~# "
 send -- "date >> timestamp.txt\r"
 expect "# "
@@ -33,3 +23,15 @@ send -- "docker-compose up -d\r"
 expect "# "
 send -- "exit\r"
 expect eof
+
+
+# spawn ssh root@104.248.194.185 'bash -s' < remote_script.sh
+# spawn ssh -T root@104.248.194.185 << _remote_commands
+# date >> timestamp.txt
+
+# rm -rf vote_the_news
+# mkdir vote_the_news
+# cd vote_the_news
+# git clone https://github.com/FelipeKha/vote_the_news.git
+
+# _remote_commands
